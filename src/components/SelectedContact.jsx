a425c8d594aa891466d27b101ff5bb91977ea3dd
JCOMET23 <gabecometa@gmail.com>
2023-06-25 22:15:46 -0700

    final commit

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-export default function SelectedContact({
-  selectedContactId,
-  setSelectedContactId,
-}) {
+export default function SelectedContact({ selectedContactId, setSelectedContactId }) {
   const [contact, setContact] = useState(null);
 
   useEffect(() => {
     const fetchContact = async () => {
       try {
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/users/${selectedContactId}`
-        );
+        const url = `https://jsonplaceholder.typicode.com/users/${selectedContactId}`;
+        const response = await fetch(url);
         const data = await response.json();
         setContact(data);
       } catch (error) {
@@ -20,25 +16,25 @@ export default function SelectedContact({
     };
 
     fetchContact();
-  }, [selectedContactId]);
+  }, []);
 
   return (
     <div>
       <h2>Selected Contact</h2>
-      {
-        contact (
-            <>
-            <p>Name: {contact.name}</p>
-            <p>Email: {contact.email}</p>
-            <p>Phone: {contact.phone}</p>
-            <p>Website: {contact.website}</p>
-            <p>Company: {contact.company.name}</p>
-            </>
-        )
-      }
-      <button onClick={() => setSelectedContactId(null)}>
-        Clear Selection
-      </button>
+      {contact ? (
+        <>
+          <p>Name: {contact.name}</p>
+          <p>Email: {contact.email}</p>
+          <p>Phone: {contact.phone}</p>
+          <p>Website: {contact.website}</p>
+          <p>Company: {contact.company.name}</p>
+        </>
+      ) : (
+        <p>Loading contact information...</p>
+      )}
+      <button onClick={() => setSelectedContactId(null)}>Clear Selection</button>
     </div>
   );
 }
+
+
